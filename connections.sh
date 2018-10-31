243b229d76c4526af618aacea0b997bcb91dde82
Andres Jaek <andres.jaek@rocksoft.ee>
2018-10-31 14:55:16 +0200

    Last minute customizations before live

--- connections.sh
+++ connections.sh
@@ -2,4 +2,5 @@ USER=SGW-eBS-test
 PASSWORD="7AzHzYWw"
 AUTHENTICATION=$USER:$PASSWORD
 TIMEOUT=500
-
+URL_PAY=https://soa-test.elion.ee/soap/Enterprise/Dynamics/SofBankAPIPayment/xppservice.svc
+URL_INV=https://soa-test.elion.ee/soap/Enterprise/Dynamics/SofEbsSalesInvoice/xppservice.svc
