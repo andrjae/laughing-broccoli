81e593f22e425ef4bb6d4e123afe5e00e0ced833
Andres Jaek <andres.jaek@rocksoft.ee>
2018-11-13 14:31:52 +0200

    Add healthchecks

--- Some general healthchecks.sql
+++ Some general healthchecks.sql
@@ -12,8 +12,11 @@ select * from xxd_ar_trx_dax_out_v
 select * from (
 select count(*) over (partition by ebs_transaction_no, trans_type) c, a.* from xxd_dax_answer a
 ) where c>1
-order by 1 desc, 3
+order by 1 desc, 3, 5 desc
 
+
+
+select max(sent_date), count(*), EBS_TRANSACTION_NO, TRANS_TYPE, CONFIRMED, L_CONF, INFO, L_INFO, WARNING, ERR, FAULT from (
 select ebs_transaction_no, trans_type, sent_date, confirmed, l_conf, info, l_info, warning, err, fault from (
 select a.*, 
 min(info) KEEP (DENSE_RANK LAST ORDER BY sent_date)  over (partition by ebs_transaction_no, trans_type) l_info,
@@ -22,6 +25,9 @@ from xxd_dax_answer a
 )
 where nvl(info,'X') != 'Töölehele sisestatud kannete arv: 1'
 or nvl(l_info,'X') != 'Töölehele sisestatud kannete arv: 1'
+)
+group by EBS_TRANSACTION_NO, TRANS_TYPE, CONFIRMED, L_CONF, INFO, L_INFO, WARNING, ERR, FAULT
+order by 1 desc
 
 select * from xxd_soap_dax_v
 
